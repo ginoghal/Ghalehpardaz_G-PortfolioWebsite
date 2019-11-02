f7d317ad1faf7d8990c2dac4e1e90328318359cf
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2019-11-02 18:31:44 -0400

    added tons of changes

--- js/main.js
+++ js/main.js
@@ -20,3 +20,48 @@ const mainNavSlide = () => {
 }
 
 mainNavSlide();
+
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = 'images/web.svg';
+images[1] = 'images/marketing.svg';
+images[2] = 'images/camera.svg';
+images[3] = 'images/video.svg';
+images[4] = 'images/music.svg';
+
+function changeImg(){
+  document.heroslider.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  } else {
+      i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+  }
+    window.onload = changeImg;
+
+
+
+var current = 0,
+    slides = document.getElementsByTagName("h3");
+
+setInterval(function() {
+  for (var i = 0; i < slides.length; i++){
+    slides[i].style.opacity = 0;
+  }
+  current = (current != slides.length - 1) ? current + 1 : 0;
+  slides[current].style.opacity = 1;
+}, 3000);
+
+
+
+
+
+
+
+
+ 
