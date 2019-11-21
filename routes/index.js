890a218d6c96dfb63b3adaa4ba14ce5f8cb54383
Van Rys <tvanrys@ATS-HR420-DMPD.fanshawec.ca>
2019-11-21 13:45:50 -0500

    fixing structur and routes

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+// a forward slash is the home route (same as index.html)
+router.get('/', (req, res) => {
+    console.log('at the home route');
+  
+  
+    res.render('home', { message: "hi there!", anothermessage: "This is easy!" } );
+    // this builds localhost:3000/views/index.html
+  })
+  
+  
+  router.get('/content', (req, res) => {
+    console.log('at the content route');
+    //res.sendFile(path.join(__dirname + '/views/contact.html'));
+    res.render('content', { message: "on content page", layout:'portfolio'})
+  })
+  
+  router.get('/about', (req, res) => {
+    console.log('at the about route');
+    //res.sendFile(path.join(__dirname + '/views/contact.html'));
+    res.render('about', { message: "on about page", layout:'about'})
+  })
+  
+  router.get('/contact', (req, res) => {
+    console.log('at the content route');
+    //res.sendFile(path.join(__dirname + '/views/contact.html'));
+    res.render('contact', { message: "on contact page", layout:'contact'})
+  })
+  
+  router.get('/users', (req, res) => {
+    sql.getConnection((err, connection) => {
+      if (err) {
+        return console.log(err.message);
+      }
+  
+      let query = `SELECT * FROM tbl_card`;
+  
+      sql.query(query, (err, rows) => {
+        connection.release();
+  
+        if (err) { return console.log(err.message) }
+  
+        console.log(rows);
+  
+        res.render('user', rows[0]);
+      })
+    })
+  })
+  
+  router.get('/portfolio', (req, res) => {
+    console.log('at the portfolio route');
+    res.send('on the portfolio page!');
+  })
+
+  module.exports = router;
