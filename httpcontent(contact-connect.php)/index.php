bd53e69ea2a7ad63ca6c2728728b3e66a8695261
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2019-12-08 00:37:09 -0500

    3 of the 4 complete

---
+++ httpcontent(contact-connect.php)/index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Form</title>
+</head>
+<body>
+<form action="data/contact.php" method="post">
+<label for="name">Name</label>
+<input id="name" type="text" name="name" placeholder="name">
+
+<label for="email">Email</label>
+<input id="email" type="email" name="email" placeholder="name">
+
+<label for="subject">Subject</label>
+<input id="subject" type="subject" name="subject" placeholder="name">
+
+<button type="submit">Submit</button>
+
+</label></form>
+    
+</body>
+</html>
