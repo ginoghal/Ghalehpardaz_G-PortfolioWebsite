599d65dfc22bc6f4d552c11aff84faa2c277340d
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2020-02-28 21:47:11 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -56,3 +56,15 @@ router.get('/', (req, res) => {
   })
 
   module.exports = router;
+
+
+  var menu = document.getElementById('menu');
+    var closeIcon = document.getElementById("closeIcon");
+
+    menu.addEventListener('click', handleMenuClick);
+
+    function handleMenuClick(event) {
+      if (event.target instanceof HTMLAnchorElement) {
+        closeIcon.checked = false;
+      }
+    }
